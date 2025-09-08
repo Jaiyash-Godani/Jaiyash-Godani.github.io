a051669e54492b9ee8255c79e3b3064cb140ad5f
Jaiyash Godani <techieboy1212@gmail.com>
2025-09-08 19:43:29 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6A00", // orange accent
+        dark: "#0A0A0A"
+      }
+    }
+  },
+  plugins: []
+}
